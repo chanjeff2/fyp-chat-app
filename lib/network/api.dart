71104d31883d996a6aedf29ba8f45c0ef6ec9a0a
chanjeff <jeffchan142@gmail.com>
2022-11-05 02:21:54 +0800

    api return map instead of response and handle bad request

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -3,24 +3,40 @@ import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
+class ApiException implements Exception {
+  int statusCode;
+  String message;
+  String error;
+
+  ApiException(this.statusCode, this.message, this.error);
+}
+
 abstract class Api {
   // use hostname -I to get wsl2 ip and replace the ip address
   static const String baseUrl = "http://192.168.81.76:3000";
   abstract String pathPrefix;
 
+  Map<String, dynamic> processResponse(http.Response response) {
+    final body = json.decode(response.body);
+    if (response.statusCode < 200 || response.statusCode >= 300) {
+      throw ApiException(response.statusCode, body["message"], body["error"]);
+    }
+    return body;
+  }
+
   @protected
-  Future<http.Response> get(
+  Future<Map<String, dynamic>> get(
     String path, {
     Map<String, String>? query,
   }) async {
     final url =
         Uri.parse("$baseUrl$pathPrefix$path").replace(queryParameters: query);
     final response = await http.get(url);
-    return response;
+    return processResponse(response);
   }
 
   @protected
-  Future<http.Response> post(
+  Future<Map<String, dynamic>> post(
     String path, {
     Map<String, dynamic>? body,
   }) async {
@@ -30,6 +46,6 @@ abstract class Api {
           'Content-Type': 'application/json; charset=UTF-8',
         },
         body: json.encode(body));
-    return response;
+    return processResponse(response);
   }
 }
