0fb582438c58f36d8a71eae91052aa0a81532601
chanjeff <jeffchan142@gmail.com>
2022-11-04 23:53:52 +0800

    fix api

--- lib/network/api.dart
+++ lib/network/api.dart
@@ -4,8 +4,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
 abstract class Api {
-  // Android is 10.0.2.2 not localhost
-  static const String baseUrl = "http://10.0.2.2:3000";
+  // use hostname -I to get wsl2 ip and replace the ip address
+  static const String baseUrl = "http://192.168.81.76:3000";
   abstract String pathPrefix;
 
   @protected
@@ -24,7 +24,7 @@ abstract class Api {
     String path, {
     Map<String, dynamic>? body,
   }) async {
-    final url = Uri.parse("$baseUrl/$pathPrefix/$path");
+    final url = Uri.parse("$baseUrl$pathPrefix$path");
     final response = await http.post(url, body: json.encode(body));
     return response;
   }
