6a28b77bdca068283a733cf9950fa129f4612168
CurlyRhapsody <72722907+CurlyRhapsody@users.noreply.github.com>
2022-12-31 00:30:58 +0800

    Improvements to Chatroom
    1. Added icon and return button
    2. Added emoji, camera and attachment icon
    3. Send button shows mic and send button depending on whether there are texts
    4. Added more options to popup menu

--- lib/screens/home/chatroom.dart
+++ lib/screens/home/chatroom.dart
@@ -3,14 +3,46 @@ import 'package:fyp_chat_app/models/user_state.dart';
 import 'package:fyp_chat_app/storage/credential_store.dart';
 import 'package:provider/provider.dart';
 
-class ChatRoomScreen extends StatelessWidget {
+class ChatRoomScreen extends StatefulWidget {
   const ChatRoomScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatRoomScreen> createState() => _ChatRoomScreenState();
+}
+
+class _ChatRoomScreenState extends State<ChatRoomScreen> {
+  final TextEditingController _messageController = TextEditingController();
+
+  bool _textMessage = false;
+  bool _emojiBoardShown = false;
+
+  String get message => _messageController.text;
 
   @override
   Widget build(BuildContext context) {
     return Consumer<UserState>(
       builder: (context, userState, child) => Scaffold(
         appBar: AppBar(
+          leading: InkWell(
+            onTap: () {
+              Navigator.pop(context);
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const [
+                Icon(
+                  Icons.arrow_back,
+                  size: 24,
+                ),
+                CircleAvatar(
+                  // child: profilePicture ?? Icon(Icons.person, size: 24),
+                  child: Icon(Icons.person, size: 24, color: Colors.white),
+                  radius: 16,
+                  backgroundColor: Colors.blueGrey,
+                ),
+              ],
+            ),
+          ),
           title: const Text("ChatRoom with"),
           actions: [
             PopupMenuButton(
@@ -30,7 +62,27 @@ class ChatRoomScreen extends StatelessWidget {
                 PopupMenuItem(
                   child: TextButton(
                     onPressed: () {},
-                    child: const Text("More..."),
+                    child: const Text("Mute Notifications"),
+                  ),
+                ),
+                PopupMenuItem(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: PopupMenuButton(
+                    child: const Text("More...", style: TextStyle(color: Colors.blue, fontSize: 14, fontWeight: FontWeight.w500),),
+                    itemBuilder: (context) => [
+                      PopupMenuItem(
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text("Report user"),
+                        ),
+                      ),
+                      PopupMenuItem(
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text("Block user"),
+                        ),
+                      ),
+                    ]
                   ),
                 ),
               ],
@@ -38,23 +90,80 @@ class ChatRoomScreen extends StatelessWidget {
           ],
         ),
         body: Column(children: <Widget>[
-          Expanded(
+          const Expanded(
             child: Text('Test Screen'),
           ),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: Row(children: const <Widget>[
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: Row(children: <Widget>[
               Flexible(
-                child: TextField(
+                child: TextFormField(
+                  controller: _messageController,
+                  style: const TextStyle(color: Colors.white),
+                  cursorColor: Colors.lime[800],
                   decoration: InputDecoration(
-                      contentPadding: EdgeInsets.all(16.0),
-                      border: OutlineInputBorder(),
-                      hintText: 'Type something...'),
+                      contentPadding: const EdgeInsets.all(16.0),
+                      filled: true,
+                      fillColor: Colors.blueGrey[900],
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(24.0)),
+                      ),
+                      focusedBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(24.0)),
+                      ),
+                      hintText: 'Message',
+                      hintStyle: TextStyle(color: Colors.grey[400]),
+                      prefixIcon: IconButton(
+                        icon: _emojiBoardShown 
+                        ? const Icon(Icons.keyboard, color: Colors.grey) 
+                        : const Icon(Icons.emoji_emotions_outlined, color: Colors.grey),
+                        onPressed: () {
+                          setState(() {
+                            _emojiBoardShown = !_emojiBoardShown;
+                          });
+                        },
+                      ),
+                      
+                      suffixIcon: (_textMessage) ? null 
+                        : Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.attach_file, color: Colors.grey),
+                              onPressed: () { },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.camera_alt, color: Colors.grey),
+                              onPressed: () {
+                                // Navigator.push(
+                                //     context,
+                                //     MaterialPageRoute(
+                                //         builder: (builder) =>
+                                //             CameraApp()));
+                              },
+                            ),
+                          ],
+                        ),
+                    ),
+                  onChanged: (text) {
+                    setState(() {
+                      _textMessage = text.isNotEmpty;
+                    });
+                  },
+                  minLines: 1,
+                  maxLines: 5,
                 ),
               ),
-              IconButton(
-                icon: Icon(Icons.send),
-                onPressed: null,
+              const SizedBox(width: 8),
+              CircleAvatar(
+                radius: 25,
+                backgroundColor: const Color(0xFF003366),
+                child: IconButton(
+                  icon: _textMessage
+                        ? const Icon(Icons.send, color: Colors.white)
+                        : const Icon(Icons.mic, color: Colors.white),
+                  onPressed: () { },
+                ),
               ),
             ]),
           ),
