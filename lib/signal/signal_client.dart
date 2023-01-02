dff4e38e2cd77d708f43ada57c21bdee92c8897b
jimmy5678998 <jimmy5678998@gmail.com>
2023-01-02 21:17:21 +0800

    Update signal_client.dart

--- lib/signal/signal_client.dart
+++ lib/signal/signal_client.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
 
 import 'package:fyp_chat_app/dto/update_keys_dto.dart';
 import 'package:fyp_chat_app/models/key_bundle.dart';
@@ -8,6 +10,7 @@ import 'package:fyp_chat_app/models/pre_key.dart';
 import 'package:fyp_chat_app/models/signed_pre_key.dart';
 import 'package:fyp_chat_app/models/user.dart';
 import 'package:fyp_chat_app/network/devices_api.dart';
+import 'package:fyp_chat_app/network/events_api.dart';
 import 'package:fyp_chat_app/network/keys_api.dart';
 import 'package:fyp_chat_app/network/users_api.dart';
 import 'package:fyp_chat_app/signal/device_helper.dart';
@@ -18,6 +21,9 @@ import 'package:fyp_chat_app/storage/disk_session_store.dart';
 import 'package:fyp_chat_app/storage/disk_signed_pre_key_store.dart';
 import 'package:fyp_chat_app/storage/message_store.dart';
 import 'package:libsignal_protocol_dart/libsignal_protocol_dart.dart';
+import 'package:fyp_chat_app/dto/send_message_dto.dart';
+import 'package:fyp_chat_app/models/send_message_dao.dart';
+import 'package:fyp_chat_app/dto/device_dto.dart';
 
 class SignalClient {
   SignalClient._();
@@ -67,6 +73,83 @@ class SignalClient {
   Future<void> sendMessage(String recipientUserId, String content) async {
     // TODO: implement this
     // use SendMessageDao.toDto(), EventsApi(), etc
+    final senderDeviceId = await DeviceInfoHelper().getDeviceId();
+    if (senderDeviceId == null) {
+      throw Exception('Sender Device Id is null');
+    }
+    final deviceList = await DevicesApi().getAllDevices();
+    deviceList.retainWhere((deviceDto) => deviceDto.id == recipientUserId);
+    bool _isSent = false;
+    DateTime sentTime = DateTime.now();
+    for (final device in deviceList) {
+      final remoteAddress = SignalProtocolAddress(
+        recipientUserId,
+        device.deviceId,
+      );
+      // check session is existed or not
+      final containsSession =
+          await DiskSessionStore().containsSession(remoteAddress);
+      if (!containsSession) {
+        // init session
+        final sessionBuilder = SessionBuilder(
+          DiskSessionStore(),
+          DiskPreKeyStore(),
+          DiskSignedPreKeyStore(),
+          DiskIdentityKeyStore(),
+          remoteAddress,
+        );
+        final keyBundleDto =
+            await KeysApi().getKeyBundle(recipientUserId, device.deviceId);
+        final keyBundle = KeyBundle.fromDto(keyBundleDto);
+        final oneTimeKey = keyBundle.deviceKeyBundles[0].oneTimeKey;
+        final signedPreKey = keyBundle.deviceKeyBundles[0].signedPreKey;
+        final retrievedPreKey = PreKeyBundle(
+          await DiskIdentityKeyStore().getLocalRegistrationId(),
+          (senderDeviceId)!,
+          oneTimeKey?.id,
+          oneTimeKey?.key,
+          signedPreKey.id,
+          signedPreKey.key,
+          signedPreKey.signature,
+          keyBundle.identityKey,
+        );
+        await sessionBuilder.processPreKeyBundle(retrievedPreKey);
+      }
+      // encrypt message
+      final remoteSessionCipher = SessionCipher(
+        DiskSessionStore(),
+        DiskPreKeyStore(),
+        DiskSignedPreKeyStore(),
+        DiskIdentityKeyStore(),
+        remoteAddress,
+      );
+      final ciphertext = await remoteSessionCipher
+          .encrypt(Uint8List.fromList(utf8.encode(content)));
+
+      // send message use EventsApi()
+      // TODO: ciphertext is CiphertextMessage type after encrypt, need PreKeySignalMessage
+      if (_isSent == false) {
+        sentTime = DateTime.now();
+        _isSent = true;
+      }
+      final message = SendMessageDao(senderDeviceId, recipientUserId,
+              device.deviceId, ciphertext, DateTime.now())
+          .toDto();
+      //mark first message sent time only
+
+      EventsApi().sendMessage(message);
+    }
+
+    // save message to disk
+    final plainMessage = PlainMessage(
+      senderUserId: /*TODO: add sender user id */,
+      senderUsername: /*TODO: add sender user name */,
+      content: content,
+      sentAt: sentTime,
+    );
+
+    final messageId = await MessageStore().storeMessage(plainMessage);
+    plainMessage.id = messageId;
   }
 
   Future<PlainMessage> processMessage(Message message) async {
