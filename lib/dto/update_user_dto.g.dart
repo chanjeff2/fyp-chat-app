431896dba9a31f1c808f53ca29744511b8c68c85
chanjeff <jeffchan142@gmail.com>
2023-01-02 21:01:40 +0800

    add update_profile endpoint

---
+++ lib/dto/update_user_dto.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'update_user_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UpdateUserDto _$UpdateUserDtoFromJson(Map<String, dynamic> json) =>
+    UpdateUserDto(
+      displayName: json['displayName'] as String?,
+    );
+
+Map<String, dynamic> _$UpdateUserDtoToJson(UpdateUserDto instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('displayName', instance.displayName);
+  return val;
+}
