e2b1d82cdc0471616592cd56383dd7d488bbd2a4
SiberiaNeko <cato58481@gmail.com>
2023-01-04 23:41:30 +0800

    delete redundent import

--- lib/screens/home/select_contact.dart
+++ lib/screens/home/select_contact.dart
@@ -1,6 +1,3 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 import 'package:fyp_chat_app/components/default_option.dart';
 import 'package:fyp_chat_app/components/contact_option.dart';
