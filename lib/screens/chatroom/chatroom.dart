ed96678772756d63fab8d83160d3f38aa5263c83
jimmy5678998 <jimmy5678998@gmail.com>
2023-01-04 22:15:15 +0800

    Update chatroom.dart

--- lib/screens/chatroom/chatroom.dart
+++ lib/screens/chatroom/chatroom.dart
@@ -3,14 +3,15 @@ import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
 import 'package:fyp_chat_app/models/user_state.dart';
 import 'package:fyp_chat_app/screens/chatroom/contact_info.dart';
+import 'package:fyp_chat_app/storage/message_store.dart';
 import 'package:provider/provider.dart';
-import 'package:fyp_chat_app/screens/chatroom/message_bubble.dart';
-
+import 'package:fyp_chat_app/signal/signal_client.dart';
 import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 
 class ChatRoomScreen extends StatefulWidget {
-  const ChatRoomScreen({Key? key}) : super(key: key);
+  const ChatRoomScreen({Key? key, required this.recipientId}) : super(key: key);
 
+  final String recipientId;
   @override
   State<ChatRoomScreen> createState() => _ChatRoomScreenState();
 }
@@ -25,22 +26,57 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
 
   String get message => _messageController.text;
 
+  /*
   void _submitMsg(String message) {
     // Get the current time
     DateTime now = DateTime.now();
-    String parsedTime = "${now.hour.toString().padLeft(2,'0')}:${now.minute.toString().padLeft(2,'0')}";
+    String parsedTime =
+        "${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}";
     //add the text into message array for temporary storage
     setState(() {
       _messages.insert(
           0,
           Container(
-              child: MessageBubble(text: message, time: parsedTime, isCurrentUser: true),
+              child: MessageBubble(
+                  text: message, time: parsedTime, isCurrentUser: true),
               alignment: Alignment.centerRight));
     });
     _messageController.clear();
     _textMessage = false;
   }
+  */
+  void _submitMsg(String message) async {
+    // send message by using signal client
+    SignalClient().sendMessage(widget.recipientId, message);
+    _messageController.clear();
+    _textMessage = false;
+    final messageList =
+        await MessageStore().getMessageByUserId(widget.recipientId);
+    print(messageList);
+  }
 
+  /*
+  void _processMsg() {
+    // get message from message store
+    _messages. await MessageStore().getMessageByUserId(widget.recipientId);
+    
+    // decorate the message with message bubble
+    
+    // Get the current time
+    DateTime now = DateTime.now();
+    String parsedTime =
+        "${now.hour.toString().padLeft(2, '0')}:${now.minute.toString().padLeft(2, '0')}";
+    //add the text into message array for temporary storage
+    setState(() {
+      _messages.insert(
+          0,
+          Container(
+              child: MessageBubble(
+                  text: message, time: parsedTime, isCurrentUser: true),
+              alignment: Alignment.centerRight));
+    });
+  }
+  */
   @override
   Widget build(BuildContext context) {
     return Consumer<UserState>(
@@ -65,7 +101,7 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                 // SizedBox(width: 4),
                 Expanded(
                   child: CircleAvatar(
-                  // child: profilePicture ? null : Icon(Icons.person, size: 48),
+                    // child: profilePicture ? null : Icon(Icons.person, size: 48),
                     child: Icon(Icons.person, size: 20, color: Colors.white),
                     radius: 32,
                     backgroundColor: Colors.blueGrey,
@@ -78,18 +114,16 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
           title: SizedBox(
             width: MediaQuery.of(context).size.width,
             child: InkWell(
-              onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => const ContactInfo()
-                          )),
+              onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => const ContactInfo())),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: const [
                   Text("Test user"), //TODO: add target name to title
-                  Text("Online",
-                    style: TextStyle(
-                      fontWeight: FontWeight.normal,
-                      fontSize: 14
-                    ),
+                  Text(
+                    "Online",
+                    style:
+                        TextStyle(fontWeight: FontWeight.normal, fontSize: 14),
                   ),
                 ],
               ),
@@ -113,28 +147,35 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                       height: 48.0, //default height
                       width: double.infinity,
                       child: Row(
-                              children: const <Widget>[
-                                Text("More"),
-                                Spacer(),
-                                Icon(Icons.keyboard_arrow_right, color: Colors.grey,),
-                              ],
-                            ),
+                        children: const <Widget>[
+                          Text("More"),
+                          Spacer(),
+                          Icon(
+                            Icons.keyboard_arrow_right,
+                            color: Colors.grey,
+                          ),
+                        ],
+                      ),
                     ),
                     itemBuilder: (innerContext) {
                       return [
                         PopupMenuItem(
                           value: 101,
                           child: const Text("Block User"),
-                          onTap: () { Navigator.of(innerContext).pop(); },
+                          onTap: () {
+                            Navigator.of(innerContext).pop();
+                          },
                         ),
                         PopupMenuItem(
                           value: 102,
                           child: const Text("Report User"),
-                          onTap: () { Navigator.of(innerContext).pop(); },
+                          onTap: () {
+                            Navigator.of(innerContext).pop();
+                          },
                         ),
                       ];
                     },
-                    ),
+                  ),
                 ),
               ],
             )
@@ -159,9 +200,8 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                   padding: const EdgeInsets.symmetric(horizontal: 12),
                   alignment: Alignment.centerLeft,
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey.shade600),
-                    borderRadius: BorderRadius.circular(16.0)
-                  ),
+                      border: Border.all(color: Colors.grey.shade600),
+                      borderRadius: BorderRadius.circular(16.0)),
                   child: Scrollbar(
                     controller: _scrollController,
                     child: TextField(
@@ -179,27 +219,31 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                         hintStyle: TextStyle(color: Colors.grey.shade600),
                         border: InputBorder.none,
                         prefixIcon: IconButton(
-                            icon: _emojiBoardShown
-                                ? Icon(Icons.keyboard, color: Colors.grey.shade600)
-                                : Icon(Icons.emoji_emotions_outlined, color: Colors.grey.shade600),
-                            onPressed: () {
-                              FocusManager.instance.primaryFocus?.unfocus();
-                              setState(() {
-                                _emojiBoardShown = !_emojiBoardShown;
-                              });
-                            },
-                          ),
+                          icon: _emojiBoardShown
+                              ? Icon(Icons.keyboard,
+                                  color: Colors.grey.shade600)
+                              : Icon(Icons.emoji_emotions_outlined,
+                                  color: Colors.grey.shade600),
+                          onPressed: () {
+                            FocusManager.instance.primaryFocus?.unfocus();
+                            setState(() {
+                              _emojiBoardShown = !_emojiBoardShown;
+                            });
+                          },
+                        ),
                         suffixIcon: (_textMessage)
                             ? null
                             : Row(
                                 mainAxisSize: MainAxisSize.min,
                                 children: [
                                   IconButton(
-                                    icon: Icon(Icons.attach_file, color: Colors.grey.shade600),
+                                    icon: Icon(Icons.attach_file,
+                                        color: Colors.grey.shade600),
                                     onPressed: () {},
                                   ),
                                   IconButton(
-                                    icon: Icon(Icons.camera_alt, color: Colors.grey.shade600),
+                                    icon: Icon(Icons.camera_alt,
+                                        color: Colors.grey.shade600),
                                     onPressed: () {
                                       // Navigator.push(
                                       //     context,
@@ -235,8 +279,10 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
                   icon: _textMessage
                       ? const Icon(Icons.send, color: Colors.white)
                       : const Icon(Icons.mic, color: Colors.white),
-                  onPressed: () => {
-                    if (message.trim().isNotEmpty) {_submitMsg(message)}
+                  onPressed: () {
+                    if (message.trim().isNotEmpty) {
+                      _submitMsg(message);
+                    }
                   },
                 ),
               ),
@@ -245,49 +291,49 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
           Offstage(
             offstage: !_emojiBoardShown,
             child: SizedBox(
-              height: 250,
-              child: EmojiPicker(
-                textEditingController: _messageController,
-                onEmojiSelected: (category, emoji) {
-                  setState(() {
-                    _textMessage = message.trim().isNotEmpty;
-                  });
-                },
-                onBackspacePressed: () {
-                  setState(() {
-                    _textMessage = message.trim().isNotEmpty;
-                  });
-                },
-                config: Config(
-                  columns: 8,
-                  emojiSizeMax: 32 * (Platform.isIOS ? 1.30 : 1.0),
-                  verticalSpacing: 0,
-                  horizontalSpacing: 0,
-                  gridPadding: EdgeInsets.zero,
-                  initCategory: Category.RECENT,
-                  bgColor: const Color(0xFFF2F2F2),
-                  indicatorColor: Theme.of(context).primaryColor,
-                  iconColor: Colors.grey,
-                  iconColorSelected: Theme.of(context).primaryColor,
-                  backspaceColor: Theme.of(context).primaryColor,
-                  skinToneDialogBgColor: Colors.white,
-                  skinToneIndicatorColor: Colors.grey,
-                  enableSkinTones: true,
-                  showRecentsTab: true,
-                  recentsLimit: 28,
-                  replaceEmojiOnLimitExceed: false,
-                  noRecents: const Text(
-                    'No Recents',
-                    style: TextStyle(fontSize: 20, color: Colors.black26),
-                    textAlign: TextAlign.center,
+                height: 250,
+                child: EmojiPicker(
+                  textEditingController: _messageController,
+                  onEmojiSelected: (category, emoji) {
+                    setState(() {
+                      _textMessage = message.trim().isNotEmpty;
+                    });
+                  },
+                  onBackspacePressed: () {
+                    setState(() {
+                      _textMessage = message.trim().isNotEmpty;
+                    });
+                  },
+                  config: Config(
+                    columns: 8,
+                    emojiSizeMax: 32 * (Platform.isIOS ? 1.30 : 1.0),
+                    verticalSpacing: 0,
+                    horizontalSpacing: 0,
+                    gridPadding: EdgeInsets.zero,
+                    initCategory: Category.RECENT,
+                    bgColor: const Color(0xFFF2F2F2),
+                    indicatorColor: Theme.of(context).primaryColor,
+                    iconColor: Colors.grey,
+                    iconColorSelected: Theme.of(context).primaryColor,
+                    backspaceColor: Theme.of(context).primaryColor,
+                    skinToneDialogBgColor: Colors.white,
+                    skinToneIndicatorColor: Colors.grey,
+                    enableSkinTones: true,
+                    showRecentsTab: true,
+                    recentsLimit: 28,
+                    replaceEmojiOnLimitExceed: false,
+                    noRecents: const Text(
+                      'No Recents',
+                      style: TextStyle(fontSize: 20, color: Colors.black26),
+                      textAlign: TextAlign.center,
+                    ),
+                    loadingIndicator: const SizedBox.shrink(),
+                    tabIndicatorAnimDuration: kTabScrollDuration,
+                    categoryIcons: const CategoryIcons(),
+                    buttonMode: ButtonMode.MATERIAL,
+                    checkPlatformCompatibility: true,
                   ),
-                  loadingIndicator: const SizedBox.shrink(),
-                  tabIndicatorAnimDuration: kTabScrollDuration,
-                  categoryIcons: const CategoryIcons(),
-                  buttonMode: ButtonMode.MATERIAL,
-                  checkPlatformCompatibility: true,
-                ),
-              )),
+                )),
           ),
         ]),
       ),
@@ -303,18 +349,22 @@ class _ChatRoomScreenState extends State<ChatRoomScreen> {
 
   _onMenuItemSelected(int value, UserState userState) {
     switch (value) {
-      case 0: {
-        break;
-      }
-      case 1: {
-        break;
-      }
-      case 2: {
-        break;
-      }
-      case 3: {
-        break;
-      }
+      case 0:
+        {
+          break;
+        }
+      case 1:
+        {
+          break;
+        }
+      case 2:
+        {
+          break;
+        }
+      case 3:
+        {
+          break;
+        }
     }
   }
 }
