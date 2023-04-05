b433cd270137e2524578679129efc18f97273c0c
SiberiaNeko <cato58481@gmail.com>
2023-04-06 01:26:52 +0800

    import the inputDialog to floating button on group creating screen

--- lib/screens/home/create_group_screen.dart
+++ lib/screens/home/create_group_screen.dart
@@ -2,14 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:fyp_chat_app/components/contact_option.dart';
 import 'package:fyp_chat_app/components/palette.dart';
+import 'package:fyp_chat_app/dto/create_group_dto.dart';
 import 'package:fyp_chat_app/models/chatroom.dart';
+import 'package:fyp_chat_app/network/api.dart';
+import 'package:fyp_chat_app/network/group_chat_api.dart';
 import 'package:fyp_chat_app/storage/chatroom_store.dart';
 import 'package:intl/intl.dart';
+
+import '../../models/group_chat.dart';
 
 class CreateGroupScreen extends StatefulWidget {
   const CreateGroupScreen({
     Key? key,
+    this.onNewChatroom,
   }) : super(key: key);
+  final void Function(Chatroom)? onNewChatroom;
 
   @override
   State<CreateGroupScreen> createState() => _CreateGroupScreen();
@@ -21,10 +28,19 @@ class _CreateGroupScreen extends State<CreateGroupScreen> {
   List<bool> isChecked = [];
   List<String> outputArray = [];
   List<Chatroom> filterList = [];
+  late TextEditingController addContactController;
 
+  @override
   void initState() {
     super.initState();
     _loadChatroomFuture = _loadChatroom();
+    addContactController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    addContactController.dispose();
+    super.dispose();
   }
 
   Future<bool> _loadChatroom() async {
@@ -34,14 +50,33 @@ class _CreateGroupScreen extends State<CreateGroupScreen> {
     });
     final listOfRooms = _chatroomMap.values.toList();
     listOfRooms.sort((a, b) => a.compareByLastActivityTime(b) * -1);
-    filterList = listOfRooms
-        .where((i) => i.type == ChatroomType.oneToOne)
-        .toList();
+    filterList =
+        listOfRooms.where((i) => i.type == ChatroomType.oneToOne).toList();
     setState(() {
       isChecked = List.filled(filterList.length, false);
     });
     return true;
   }
+
+  Future<String?> inputDialog(String title, String hint) => showDialog<String>(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text(title),
+          content: TextField(
+            autofocus: true,
+            decoration: InputDecoration(hintText: hint),
+            controller: addContactController,
+          ),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(addContactController.text);
+                  addContactController.clear();
+                },
+                child: const Text('Submit'))
+          ],
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
@@ -66,8 +101,10 @@ class _CreateGroupScreen extends State<CreateGroupScreen> {
                   });
                 },
                 child: ListTile(
-                  contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                  tileColor: isChecked[index] ? Palette.ustGrey[500] : Colors.white,
+                  contentPadding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  tileColor:
+                      isChecked[index] ? Palette.ustGrey[500] : Colors.white,
                   leading: const CircleAvatar(
                     child: Icon(Icons.person, size: 28, color: Colors.white),
                     radius: 28,
@@ -94,14 +131,30 @@ class _CreateGroupScreen extends State<CreateGroupScreen> {
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
+        onPressed: () async {
           outputArray = filterList
-              .where((element) =>
-                isChecked[filterList.indexOf(element)]
-              )
+              .where((element) => isChecked[filterList.indexOf(element)])
               .map(((e) => e.name))
               .toList();
-          print(outputArray);
+          final name = await inputDialog(
+            "Add Group",
+            "Please enter the Group name",
+          );
+          if (name == null || name.isEmpty) return;
+          // create group on server
+          late final GroupChat group;
+          try {
+            group =
+                await GroupChatApi().createGroup(CreateGroupDto(name: name));
+          } on ApiException catch (e) {
+            ScaffoldMessenger.of(context)
+                .showSnackBar(SnackBar(content: Text("error: ${e.message}")));
+          }
+          await ChatroomStore().save(group);
+          // callback and return to home
+          Navigator.of(context).pop();
+          Navigator.of(context).pop();
+          widget.onNewChatroom?.call(group);
         },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
@@ -109,4 +162,3 @@ class _CreateGroupScreen extends State<CreateGroupScreen> {
     );
   }
 }
-
